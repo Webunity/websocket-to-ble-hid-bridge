46d4fff4c9930629df2a196c89307b6a0357122e
Gilles van den Hoven <gilles@webunity.nl>
2019-04-17 07:41:43 +0200

    Fixed compile issues after refactoring

--- make-libusb.sh
+++ make-libusb.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-SETUP_DIR=`pwd`
 
 echo '------------------------------------------'
 echo 'Setting up: websocket-to-ble-hid-bridge'
@@ -12,37 +11,32 @@ echo 'Starting with setup in 5 seconds...'
 echo
 sleep 5
 
+SETUP_DIR=`pwd`
+export BTSTACK_ROOT="${SETUP_DIR}/assets/btstack"
+
 if [ ! -d "assets" ]; then
 	mkdir assets
 	sudo apt-get install git gcc libusb-1.0 pkg-config
 fi
 
 if [ ! -d "assets/btstack" ]; then
-	echo '- Cloning "btstack" library ...';
+	echo '- Cloning "btstack" library ...'
 	git clone -b develop --single-branch --depth 1 --quiet https://github.com/bluekitchen/btstack assets/btstack
 	if [ $? == 1 ]; then
 		echo '  - Error running GIT command, exiting!'
 		exit 1
 	fi
-fi
 
-if grep -q 'BTSTACK_ROOT = ../..' ./assets/btstack/port/libusb/Makefile; then
-	echo '- Compiling "btstack" library ...';
+	echo '- Compiling "btstack" library ...'
 	cd ./assets/btstack/port/libusb && make
 	if [ $? == 1 ]; then
 		echo '  - Error building btstack, exiting!'
 		exit 1
 	fi
-	cd $SETUP_DIR
-
-	# Make sure btstack can be included from our project and compilation of examples remain working.
-	echo '- Patching "libusb" port Makefile to enable compilation ...';
-	sed -i '2s;^;CWD:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))\n;' ./assets/btstack/port/libusb/Makefile
-	sed -i 's/BTSTACK_ROOT = /BTSTACK_ROOT = $(CWD)\//g' ./assets/btstack/port/libusb/Makefile
 fi
 
 if [ ! -d "assets/mongoose" ]; then
-	echo '- Cloning "mongoose" library ...';
+	echo '- Cloning "mongoose" library ...'
 	git clone --single-branch --depth 1 --quiet https://github.com/cesanta/mongoose assets/mongoose
 	if [ $? == 1 ]; then
 		echo '  - Error running GIT command, exiting!'
@@ -50,9 +44,9 @@ if [ ! -d "assets/mongoose" ]; then
 	fi
 fi
 
-echo '- (re)Compiling ...';
+echo '- (re)Compiling ...'
 echo
-make
+cd "${SETUP_DIR}/libusb" && make
 if [ $? == 1 ]; then
 	echo '  - Error building btstack, exiting!'
 	exit 1
